41e04d835b5311a3143ef6289c4e701d54f6e537
Joshua Gardner <102574879@student.swin.edu.au>
2021-12-08 23:04:57 +1100

    Rename 2021 Day 1 classname

--- src/it/jgardner/aoc/t21/day1/T21Day1.java
+++ src/it/jgardner/aoc/t21/day1/T21Day1.java
@@ -2,13 +2,12 @@ package it.jgardner.aoc.t21.day1;
 
 import it.jgardner.aoc.utils.AOCUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-class T21Day1Part1 {
+class T21Day1 {
     public static void main(String[] args) {
         List<String> input = AOCUtils.getInputData(2021, 1);
         List<Integer> intInput = input.stream().map(Integer::parseInt).collect(Collectors.toList());
